50699a1358b37cb81a67844070ecf135835d4bf8
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-05-12 19:25:35 +0530

    Updates

---
+++ precache-manifest.edd3a308ced74464f9bef1cf22408793.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5457783635cbfdc3f47bf849914815",
+    "url": "/neurostack/index.html"
+  },
+  {
+    "revision": "85cd655403ca39f97260",
+    "url": "/neurostack/static/css/main.456ec486.chunk.css"
+  },
+  {
+    "revision": "bb816612b94c2ee8eefe",
+    "url": "/neurostack/static/js/2.1cde16f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/neurostack/static/js/2.1cde16f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cd655403ca39f97260",
+    "url": "/neurostack/static/js/main.d61f841d.chunk.js"
+  },
+  {
+    "revision": "fd4d45c4d841413ae828",
+    "url": "/neurostack/static/js/runtime-main.6cf2c14c.js"
+  },
+  {
+    "revision": "1ee4090de814fb6b72eb91801b1eb944",
+    "url": "/neurostack/static/media/doctors_greetings.1ee4090d.svg"
+  },
+  {
+    "revision": "a2765bfc360683f452cd927031642daf",
+    "url": "/neurostack/static/media/logo.a2765bfc.svg"
+  }
+]);
--- src/shared/greet_data.js
+++
@@ -1,7 +0,0 @@
-const greetData = {
-    title: "Welcome to the portal where you can find best resources on Neurology.",
-    subtitle: "The resources attached to this website are totally belong to Dr. Mausamin Hathidara.",
-    image_path: "doctors_greetings.svg",
-}
-
-export default greetData;
