6027b2825b69347e7a99fb752f22c407f696e1c9
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-05-12 19:19:34 +0530

    ghpages branch created

--- src/components/Greetings/Greetings.js
+++ src/components/Greetings/Greetings.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Fade} from 'react-reveal';
 import './Greetings.css';
-// import greetData from '../../shared/greet_data.js';
 
 class Greetings extends Component {
     render(){
